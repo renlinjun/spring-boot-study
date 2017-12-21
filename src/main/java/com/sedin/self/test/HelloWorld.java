29ce02143a070f27ff3c274a9220eeeefe519c67
renlinjun <renlinjun89@163.com>
2017-12-21 17:01:27 +0800

    添加了日志功能

--- src/main/java/com/sedin/self/test/HelloWorld.java
+++ src/main/java/com/sedin/self/test/HelloWorld.java
@@ -1,11 +1,11 @@
 package com.sedin.self.test;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * 作者：REN
@@ -16,18 +16,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 //@EnableAutoConfiguration
 @SpringBootApplication
 public class HelloWorld {
+    private static Logger log = LoggerFactory.getLogger(HelloWorld.class);
 
 //    public static void main(String[] args) {
 //        System.out.println("Hello World!");
 //    }
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(HelloWorld.class, args);
+        log.info("hello world=============");
+        ConfigurableApplicationContext run = SpringApplication.run(HelloWorld.class, args);
     }
 }
