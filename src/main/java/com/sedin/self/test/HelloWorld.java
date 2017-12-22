6d40dca529d81efa767737ed8fb09f250be25ea2
renlinjun <renlinjun89@163.com>
2017-12-22 17:40:10 +0800

    添加了日志功能

--- src/main/java/com/sedin/self/test/HelloWorld.java
+++ src/main/java/com/sedin/self/test/HelloWorld.java
@@ -3,6 +3,7 @@ package com.sedin.self.test;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -14,9 +15,22 @@ import org.springframework.context.ConfigurableApplicationContext;
  */
 //@Controller
 //@EnableAutoConfiguration
+//
+
+/*@Configuration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@ComponentScan*/
+
+/*
+    exclude是针对springBoot自己的类，采用自动装配的机制，
+    告知应用程序，某些类不需要装配，个人理解我不需要加载到应用程序中
+*/
 @SpringBootApplication
 public class HelloWorld {
     private static Logger log = LoggerFactory.getLogger(HelloWorld.class);
+
+    @Autowired
+    public NoAutoConfigClass noAuto;
 
 //    public static void main(String[] args) {
 //        System.out.println("Hello World!");
